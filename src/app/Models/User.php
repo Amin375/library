5e5d587edc8f22631ae1d28538afc5121a5ecf1f
amin1999 <79565@roc-teraa.nl>
2022-01-13 10:37:11 +0100

    user model fix

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
